0d00e75f5190cf45722f64661d0ab72c5937dd11
Matthew Rasmussen <mrasmus@gmail.com>
2014-01-21 23:16:34 -0800

    Made dataset initializers more generic-looking.

--- genpyrange.py
+++ genpyrange.py
@@ -1,7 +1,7 @@
-from string import upper
+prefix = '00-00-00-00'
 output = '['
 for i in range(21,25):
   for j in range(256):
-    output = output + ('{"model": "MACLibrary.address", "fields": {"mac": "00-1F-11-02-%02X-%02X"}},' % (i,j))
+    output = output + ('{"model": "MACLibrary.address", "fields": {"mac": "' + prefix + '-%02X-%02X"}},' % (i,j))
 output = output[:-1] + ']'
 print output
